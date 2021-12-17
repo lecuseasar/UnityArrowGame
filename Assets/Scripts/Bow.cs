33ab467cb9f66fbd7fbd52700d592b0f8c47d852
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-12-17 09:24:20 +0300

    Add future select arrow type 1,2,3

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class Bow : MonoBehaviour
 {
     public GameObject arrowGameOject;
+
+    public GameObject arrowGameOject2;
     public Slider sliderObject;
 
     public float stretch;
     public float stretchSpeed = 5;
     public float stretchMax = 50;
 
+    private int itemNumbers = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -28,18 +31,34 @@ public class Bow : MonoBehaviour
             sliderObject.value = stretch;
 
         }
-
         if (Input.GetMouseButtonUp(0))
         {
-            ShootArrow(stretch);
-            stretch = 0;
-            sliderObject.value = 0;
+            if (itemNumbers == 1)
+            {
+                ShootArrow(arrowGameOject, stretch);
+                stretch = 0;
+                sliderObject.value = 0;
+            }
+            if (itemNumbers == 2)
+            {
+                ShootArrow(arrowGameOject2, stretch);
+                stretch = 0;
+                sliderObject.value = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            itemNumbers = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            itemNumbers = 2;
         }
     }
-    void ShootArrow(float arrowSpeed)
+    void ShootArrow(GameObject gameObject, float arrowSpeed)
     {
         Camera camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        GameObject arrow = Instantiate(arrowGameOject, camera.transform.position, camera.transform.rotation);
+        GameObject arrow = Instantiate(gameObject, camera.transform.position, camera.transform.rotation);
         arrow.transform.Rotate(0, 90, 0);
         arrow.GetComponent<Rigidbody>().velocity = camera.transform.forward * arrowSpeed;
     }
--- Assets/Scripts/SmoothMouseLook.cs
+++ Assets/Scripts/SmoothMouseLook.cs
@@ -9,6 +9,8 @@ public class SmoothMouseLook : MonoBehaviour
     Vector2 _mouseAbsolute;
     Vector2 _smoothMouse;
 
+    public GameObject playerGameObject;
+
     public Vector2 clampInDegrees = new Vector2(360, 180);
     public bool lockCursor;
     public Vector2 sensitivity = new Vector2(2, 2);
@@ -57,7 +59,7 @@ public class SmoothMouseLook : MonoBehaviour
         var yRotation = Quaternion.AngleAxis(_mouseAbsolute.x, transform.InverseTransformDirection(Vector3.up));
         transform.localRotation *= yRotation;
         transform.rotation *= targetOrientation;
+
+        //playerGameObject.transform.rotation *= targetOrientation;
     }
-
-
 }
